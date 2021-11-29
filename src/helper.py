ada0907dc777f947f51926eacf98f98b1ba4cf3f
Williams, Reilly (DSS) <reilly.williams@disneystreaming.com>
2021-11-29 15:59:37 -0500

    redo dataset creation

--- src/helper.py
+++ src/helper.py
@@ -48,44 +48,79 @@ About
 
 
 def get_image(data_about):
-
-    img = data_about.find(class_="portrait").find("img")
-    if img:
-        return dict(img=img["src"])
-    else:
-        return dict(img=None)
+    if data_about.find(class_="portrait"):
+        img = data_about.find(class_="portrait").find("img")
+        if img:
+            return dict(img=img["src"])
+        else:
+            return dict(img=None)
+    else: return dict(img=None)
 
 
 def get_name_real_name(data_about):
-    name = data_about.find("h1").text
-    real_name = data_about.find("h2").text
+    if data_about.find("h1"):
+        name = data_about.find("h1").text
+        real_name = data_about.find("h2").text
+    else:
+        name = real_name = ""
     return dict(name=name, real_name=real_name)
 
 
 def get_overall_score(data_about):
-    return dict(overall_score=data_about.find(href="#class-info").text)
+    if data_about.find(href="#class-info"):
+        return dict(overall_score=data_about.find(href="#class-info").text)
+    else: return dict(overall_score="")
 
+
+# def get_power_stats(data_about):
+#     #print(data_about)
+#     scripts = data_about.findAll("script") or ["Error"]
+#     script = ''
+#     #print(scripts)
+#     # Find script containng the 'stats_shdb'
+#     if scripts != ['Error']:
+#         for s in scripts:
+#             if s.text.strip().startswith("var stats_shdb = ["):
+#                 script = s
+#         # script = [s.text for s in scripts if s.text.strip().startswith("var stats_shdb = [")]
+# #         script = next(
+# #             (s.text for s in scripts if s.text.strip().startswith("var stats_shdb = ["))
+# #         )
+#         # Extract the list of powers
+#         values = re.findall(r"(\d+)", script.split(";")[0]) or [0]
+#         values = [int(v) for v in values]
+        
+#         labels = data_about.find(class_="stat-holder").findAll("label") or ["Error"]
+#         labels = [l.text for l in labels]
+#     else:
+#         labels, values = []
+
+#     return dict(zip(labels, values))
 
 def get_power_stats(data_about):
-
-    scripts = data_about.findAll("script")
-    # Find script containng the 'stats_shdb'
-    script = next(
-        (s.text for s in scripts if s.text.strip().startswith("var stats_shdb = ["))
-    )
-    # Extract the list of powers
-    values = re.findall(r"(\d+)", script.split(";")[0])
-    values = [int(v) for v in values]
-
-    labels = data_about.find(class_="stat-holder").findAll("label")
-    labels = [l.text for l in labels]
-
-    return dict(zip(labels, values))
+    script = ''
+    if data_about.findAll("script"):
+        scripts = data_about.findAll("script")
+        for s in scripts:
+            if s.text.strip().startswith("var stats_shdb = ["):
+                script = s
+        # Extract the list of powers
+        values = re.findall(r"(\d+)", script.split(";")[0]) or [0]
+        values = [int(v) for v in values]
+        
+        if data_about.find(class_="stat-holder"):
+            labels = data_about.find(class_="stat-holder").findAll("label") or ["Error"]
+            labels = [l.text for l in labels]
+        else: labels = []
+        return dict(zip(labels, values))
+    else: return dict(zip([], []))
 
 
 def get_super_powers(data_about):
-    superpowers = data_about.find("h3", text="Super Powers").findParent().findAll("a")
-    superpowers = [s.text for s in superpowers]
+    superpowers = data_about.find("h3", text="Super Powers")
+    if superpowers:
+        superpowers= superpowers.findParent().findAll("a")
+        superpowers = [s.text for s in superpowers]
     return dict(superpowers=superpowers)
 
 
@@ -96,43 +131,48 @@ def get_all_links(td):
 
 
 def get_origin(data_about):
+    if data_about.find("h3", text="Origin"):
+        data = data_about.find("h3", text="Origin").findNext()
 
-    data = data_about.find("h3", text="Origin").findNext()
+        origin = {}
 
-    origin = {}
+        for row in data.find_all("tr"):
+            key = row.find_all("td")[0].text
+            value = row.find_all("td")[1]
 
-    for row in data.find_all("tr"):
-        key = row.find_all("td")[0].text
-        value = row.find_all("td")[1]
-
-        if "alter egos" in key.lower():
-            origin[key] = get_all_links(value)
-        else:
-            origin[key] = value.text
-    return origin
+            if "alter egos" in key.lower():
+                origin[key] = get_all_links(value)
+            else:
+                origin[key] = value.text
+        return origin
+    else: return {}
 
 
 def get_connections(data_about):
-    data = data_about.find("h3", text="Connections").findNext()
+    if data_about.find("h3", text="Connections"):
+        data = data_about.find("h3", text="Connections").findNext()
 
-    connections = {}
+        connections = {}
 
-    for row in data.find_all("tr"):
-        key = row.find_all("td")[0].text
-        value = row.find_all("td")[1]
+        for row in data.find_all("tr"):
+            key = row.find_all("td")[0].text
+            value = row.find_all("td")[1]
 
-        if "Teams" in key:
-            connections[key] = get_all_links(value)
-        else:
-            connections[key] = value.text
+            if "Teams" in key:
+                connections[key] = get_all_links(value)
+            else:
+                connections[key] = value.text
+    else: connections = {}
 
     return connections
 
 
 def get_appearance(data_about):
-    table = data_about.find("h3", text="Appearance").findParent()
-    labels = table.findAll(class_="table-label")
-    return dict([(l.text, l.findNext().text) for l in labels])
+    if data_about.find("h3", text="Appearance"):
+        table = data_about.find("h3", text="Appearance").findParent()
+        labels = table.findAll(class_="table-label") or ["Error"]
+        return dict([(l.text, l.findNext().text) for l in labels])
+    else: return dict([])
 
 
 """
@@ -142,9 +182,12 @@ History
 
 def get_history(data_history):
     content = data_history.find(class_="text-columns-2")
-    title = content.find("h3").text
-    subtitles = [s.text for s in content.findAll("h4")]
-    content = " ".join([p.text for p in content.findAll("p")]).replace("\s+", " ")
+    if content:
+        title = content.find("h3").text
+        subtitles = [s.text for s in content.findAll("h4")]
+        content = " ".join([p.text for p in content.findAll("p")]).replace("\s+", " ")
+    else:
+        title = subtitles = content = ""
     return {"hist_title": title, "hist_subtitles": subtitles, "hist_content": content}
 
 
@@ -154,15 +197,22 @@ Powers
 
 
 def get_powers(data_powers):
-    content = data_powers.find_all(class_="col-8")[1]
-    title = content.find("h3").text
-    subtitles = [s.text for s in content.findAll("h4")]
-    content = " ".join([p.text for p in content.findAll("p")]).replace("\s+", " ")
-    return {
-        "powers_title": title,
-        "powers_subtitles": subtitles,
-        "powers_content": content,
-    }
+    if len(data_powers.find_all(class_="col-8")) > 1:
+        content = data_powers.find_all(class_="col-8")[1]
+        title = content.find("h3").text
+        subtitles = [s.text for s in content.findAll("h4")] or ["Error"]
+        content = " ".join([p.text for p in content.findAll("p")]).replace("\s+", " ") or ["Error"]
+        return {
+            "powers_title": title,
+            "powers_subtitles": subtitles,
+            "powers_content": content,
+        }
+    else:
+        return {
+            "powers_title": "",
+            "powers_subtitles": "",
+            "powers_content": ""
+        }
 
 
 """
